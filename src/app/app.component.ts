1c334587d76753d5dfac4a82a22743a0432ac166
Galni56 <usman.usamov.56@gmail.com>
2021-10-23 17:21:08 +0300

    Angular_Start testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test';
+  userDate = '12/04/1901';
+  phoneNum = '+405650545';
+
+  userAdresa = {
+    streetNum:'Pushkina',
+    houseNum :'Kolotushkina'
+  }
+
+  getName = () => {
+    return this.userDate;
+  }
+   getAlert() {
+    alert('test')
+   }
+  isButtonDisabled = true
+  imgUrl = "https://www.google.com.ua/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+  changed(value: any) {
+    console.log(value);
+    console.log('changed', value.target.value)
+    this.title = value.target.value;
+  }
+
+
+
 }

e0f7982172e0049e42c5299dff6834ba0059cab0
Galni56 <usman.usamov.56@gmail.com>
2021-10-31 00:14:40 +0300

    Angular_Start Pipe - Positive only Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule }  from '@angular/material/toolbar';
-import { MatButtonModule }  from '@angular/material/button';
-import { MatProgressSpinnerModule }  from '@angular/material/progress-spinner';
 import { PipeComponent } from './views/pipe/pipe.component';
 import { GameComponent } from './views/game/game.component';
 import { NavMenuComponent } from './shared/nav-menu/nav-menu.component';
@@ -20,6 +9,17 @@ import { NavMenuItemComponent } from './shared/nav-menu/nav-menu-item/nav-menu-i
 import { ListComponent } from './shared/list/list.component';
 import { ReverseStrPipe } from './views/pipe/reverse-str.pipe';
 import { PositiveNumOnly } from './positive-num-only.directive';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule }  from '@angular/material/toolbar';
+import { MatButtonModule }  from '@angular/material/button';
+import { MatProgressSpinnerModule }  from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -42,7 +42,9 @@ import { PositiveNumOnly } from './positive-num-only.directive';
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
